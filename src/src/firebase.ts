e1f636eb71660b6cc8c733a4bd63c90589333156
Andrew Leader <followthatleader@outlook.com>
2020-12-12 07:39:51 -0800

    Introduce caching

---
+++ src/src/firebase.ts
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyCoZPDbvDF1RVsQaoEtukRnLHAVic7_YmQ",
+  authDomain: "skiweather-6c945.firebaseapp.com",
+  projectId: "skiweather-6c945",
+  storageBucket: "skiweather-6c945.appspot.com",
+  messagingSenderId: "183370202218",
+  appId: "1:183370202218:web:22adbc07537f6126f7de13"
+};
+firebase.initializeApp(config);
+export default firebase;
